74d33d3a8af60180353ec9cfb3b9d0c0fddfee18
Ian Mathews <ian505c5@gmail.com>
2013-11-13 22:57:49 -0500

    got a somewhat decent build going

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,6 @@
 module.exports = function(grunt){
 	var jsFiles = [
-				'build/js/libs/jquery-1.8.3.js',
-				'build/js/libs/bootsrap.js',
-				'build/js/libs/handlebars.js',
-				'build/js/libs/ember-1.0.0-rc.1.js',
-				'build/js/app.js',
-				'build/js/controllers/**/*.js',
-				'build/js/models/**/*.js',
-				'build/js/views/**/*.js',
-				'build/js/ajax_helpers.js',
-				'build/js/router.js'
+				
 	]
 	//Project Configuration
 	grunt.initConfig({
@@ -19,7 +10,7 @@ module.exports = function(grunt){
 				src: ['build']
 			},
 			scripts: {
-				src: ['build/**/*.js', '!build/js/main.min.js']
+				src: ['build/**/*.js', '!build/js/main.js']
 			}
 		},
 		copy: {
@@ -30,15 +21,14 @@ module.exports = function(grunt){
 				expand: true
 			},
 		},
-		uglify: {
-			build: {
-				options: {
-					mangle: false
-				},
-				files: {
-					'build/js/main.min.js': jsFiles
-				}
+		useminPrepare: {
+			html: 'public/index.html',
+			options: {
+				dest: 'public'
 			}
+		},
+		usemin: {
+			html: 'build/index.html'
 		},
 		watch: {
 			scripts: {
@@ -65,17 +55,19 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-usemin');
 
 	grunt.registerTask(
 		'build',
 		'Compliles all of the assets and copies the files to the build directory',
-		['clean:build','copy','scripts']
+		['clean:build','scripts','copy']
 	);
 
 	grunt.registerTask(
 		'scripts',
 		'Compile JS files',
-		['uglify','clean:scripts']
+		['useminPrepare','concat','uglify','usemin','clean:scripts']
 	);
 	grunt.registerTask(
 		'node-server',
