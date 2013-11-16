a8fffd5d2e74e9af26432387669d19396e84d317
Ian Mathews <ianmathews@Ians-MacBook-Pro.local>
2013-11-16 01:56:08 -0500

    yo

--- .tmp/concat/js/main.js
+++ .tmp/concat/js/main.js
@@ -40544,7 +40544,7 @@ TestApp = Ember.Application.createWithMixins({});
 
 TestApp.ApplicationController = Ember.Controller.extend({
   init: function(){
-  	var socket = io.connect('http://localhost:3000');
+  	var socket = io.connect('http://embernode.herokuapp.com/');
 
 	var mostRecent = function(){
 		var that = this;
