d5fda7896cfb6fd5b378b7ef5207f665a4cb1b66
Ian Mathews <ianmathews@Ians-MacBook-Pro.local>
2013-11-16 02:10:25 -0500

    yoyo

--- public/js/controllers/application.js
+++ public/js/controllers/application.js
@@ -18,7 +18,7 @@ TestApp.ApplicationController = Ember.Controller.extend({
 	    var self = this;	    
 	    socket.on('show', function(data) {
 	        var url = JSON.parse(data);
-	        $(data.data).each(function(){
+	        $(url.data).each(function(){
 	        	var img = document.createElement('img');
 				$(img).attr('src', this.images.standard_resolution.url);
 				$('.images-container').append(img);
